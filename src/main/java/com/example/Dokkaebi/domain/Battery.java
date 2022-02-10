68cf634c35b9061fdd0e76fe4a1269286fdeea0a
star-sil <kimse9811@naver.com>
2022-02-10 12:38:28 +0900

    기본 생성자 추가 battery 도메인

--- src/main/java/com/example/Dokkaebi/domain/Battery.java
+++ src/main/java/com/example/Dokkaebi/domain/Battery.java
@@ -1,5 +1,7 @@
 package com.example.Dokkaebi.domain;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +10,7 @@ import java.math.BigDecimal;
 
 
 @Entity(name = "stable")
+@NoArgsConstructor
 public class Battery {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
