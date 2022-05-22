3ebfb419dab747ccccbfa80a41006d674eceb10e
star-sil <kimse9811@naver.com>
2022-05-22 19:04:48 +0900

    UPDATE 주행 경로 및 현재 배터리 용량 추가 반환
    --
    
    김성은

--- src/main/java/com/example/Dokkaebi/controller/dtos/DriveLogDto.java
+++ src/main/java/com/example/Dokkaebi/controller/dtos/DriveLogDto.java
@@ -1,20 +1,38 @@
 package com.example.Dokkaebi.controller.dtos;
 
+import com.example.Dokkaebi.domain.DriveLog;
+import com.example.Dokkaebi.domain.Scooter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 public class DriveLogDto {
     private LocalDateTime startTime;
     private double driveDist;
-    private double lat;
-    private double lon;
+    private int soc;
+    private List<Route> route = new ArrayList<>();
 
-    public DriveLogDto(LocalDateTime startTime, double driveDist, double lat, double lon) {
-        this.startTime = startTime;
-        this.driveDist = driveDist;
-        this.lat = lat;
-        this.lon = lon;
+    public DriveLogDto(DriveLog driveLog) {
+        this.startTime = driveLog.getStartTime();
+        this.driveDist = driveLog.calculateDist();
+
+        for (Scooter scooter : driveLog.getScooters()) {
+            this.soc = scooter.getSoc();
+            route.add(new Route(scooter.getLat(),scooter.getLon()));
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    private class Route {
+        double lat;
+        double lon;
     }
 }
+
+
