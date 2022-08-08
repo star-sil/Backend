e68784aa52fd8ad907bd266f5dd88ef2383ab0ac
star-sil <kimse9811@naver.com>
2022-08-08 23:07:37 +0900

    FIX CORS 에러 해결
    refresh_token을 헤더에 넣는것을 허용해주지 않아 오류가 발생했다. 따라서 refresh_token을 추가해주었다.
    
    김성은

--- src/main/java/com/example/Dokkaebi/config/SecurityConfig.java
+++ src/main/java/com/example/Dokkaebi/config/SecurityConfig.java
@@ -104,8 +104,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "refresh_token"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
