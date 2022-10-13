112706a62ffa21a93acb1202a8f8a72efdb5e6c7
star-sil <kimse9811@naver.com>
2022-10-13 23:57:03 +0900

    UPDATE api 변경으로 인한 반환 데이터 수정
    --
    
    김성은

--- src/main/java/com/example/Dokkaebi/scooter/dto/ScooterRentalStateResDto.java
+++ src/main/java/com/example/Dokkaebi/scooter/dto/ScooterRentalStateResDto.java
@@ -1,24 +1,32 @@
 package com.example.Dokkaebi.scooter.dto;
 
 import com.example.Dokkaebi.rental.Rental;
+import com.example.Dokkaebi.scooter.entity.ScooterState;
 import com.example.Dokkaebi.scooter.entity.Status;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
 public class ScooterRentalStateResDto {
+    private Long rentalId;
     private String bikeNum;
     private Status status;
     private LocalDate startDate;
     private LocalDate endDate;
+    private double lat;
+    private double lng;
+    private int soc;
 
-    public ScooterRentalStateResDto(Rental rental) {
-        this.status = rental.getScooterState().getStatus();
+    public ScooterRentalStateResDto(Rental rental, ScooterState scooterState) {
+        this.rentalId = rental.getId();
+        this.status = rental.getScooter().getStatus();
         this.startDate = rental.getStartDate();
         this.endDate = rental.getEndDate();
-        this.bikeNum = rental.getScooterState().getIdentity();
+        this.bikeNum = rental.getScooter().getIdentity();
+        this.lat = scooterState.getLat();
+        this.lng = scooterState.getLon();
+        this.soc = scooterState.getSoc();
     }
 
     public ScooterRentalStateResDto(Status status) {
